ff71b221ea5a490902d3359b818e78c8861fea49
Marcelo <marcelodrumond223@gmail.com>
2022-06-01 15:37:59 -0300

    criar menu da calculadora

---
+++ Calculadora.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+float valor1, valor2, resultado;
+int operador;
+
+int main()
+{
+	printf("Bem Vindo a Calculadora\n");
+	printf("------------------------------------------------------\n");
+	printf("Entre com os valores:");
+	scanf("%f",&valor1);
+	scanf("%f",&valor2);
+	printf("------------------------------------------------------\n");
+	printf("1- Soma\n");
+	printf("2- Subtração\n");
+	printf("3- Divisao\n");
+	printf("4- Multiplicação\n");
+	printf("5- Divisao inteira\n");
+	printf("6- Exponencial\n");
+	scanf("%d",&operador);
+	printf("------------------------------------------------------\n");
+
+	switch(operador){
+		case(1):
+			resultado=valor1+valor2;
+			printf("Resultado:%2.f\n",resultado );
+			break;
+
+		case(2):
+			break;
+
+		case(3):
+			break;
+
+		case(4):
+			break;
+
+		case(5):
+			break;
+
+		case(6):
+			break;
+			
+	}
+
+	
+	return 0;
+}
