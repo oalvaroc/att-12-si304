828a6e56cb348d947cb10beeeb40b05f8f62b6f2
ulissescardos <u244902@dac.unicamp.br>
2022-06-01 16:16:44 -0300

    using cli args in function divisao

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 double divisao(double x, double y);
@@ -7,7 +8,13 @@ int main(int argc, char const *argv[])
 {
     printf("%s\t%d\n", argv[0], argc);
 
-    printf("%lf\n", divisao(9, 4));
+    double numbers[argc];
+
+    for(int i = 0; i < argc; i++){
+        numbers[i-1] = atof(argv[i]);
+    }
+
+    printf("%lf\n", divisao(numbers[0], numbers[1]));
 
     
     return 0;
